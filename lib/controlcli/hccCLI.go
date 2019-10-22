19d40bb0eec522ad269e56bd25e91d90293f8850
EdwardSon <ehdwn1991@gmail.com>
2019-10-22 23:28:16 +0900

    status, add N nodes add Function 추가

--- lib/controlcli/hccCLI.go
+++ lib/controlcli/hccCLI.go
@@ -4,57 +4,102 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strconv"
 	"strings"
 )
 
+//ParedAction : action parser struct
+type ParedAction struct {
+	area        string
+	class       string
+	scope       []string
+	rangeoption bool
+}
+
+var tokenaction ParedAction
 var nodemap map[string]string
 
-// nodeOnlineCheck : check
-func nodeOnlineCheck() {
-	for key, val := range nodemap {
-		fmt.Println(key, val)
+// ActionParser : Parcing Action
+func ActionParser(action string) {
+
+	tmpstr := strings.Split(action, " ")
+	tokenaction.area = tmpstr[1]
+	tokenaction.class = tmpstr[2]
+	hasOption := strings.Contains(action, "-n")
+	hasRangeOption := strings.Contains(action, ":")
+	tokenaction.rangeoption = hasRangeOption
+	//deliIndex : delimeter index
+	var deliIndex int
+	var endOfIndex int
+	if hasOption {
+		if hasRangeOption {
+			for i, tmpact := range tmpstr {
+				if tmpact == "-n" {
+					deliIndex = i
+				}
+				endOfIndex++
+			}
+			tokenaction.scope = append(tokenaction.scope, tmpstr[deliIndex+1])
+		} else {
+			for i, tmpact := range tmpstr {
+				if tmpact == "-n" {
+					deliIndex = i
+				}
+				endOfIndex++
+			}
+			for t := deliIndex + 1; t < endOfIndex; t++ {
+				tokenaction.scope = append(tokenaction.scope, tmpstr[t])
+			}
+		}
+
 	}
+	//Debug : tokenaction Structure
+	// fmt.Println("area =>", tokenaction.area)
+	// fmt.Println("class => ", tokenaction.class)
+	// fmt.Println("scope => ", tokenaction.scope)
 
 }
 
 // HccCli : Hcc integration Command line interface
 func HccCli(action string) interface{} {
-	parseaction := strings.Split(action, " ")
-	area := parseaction[1]
-	actclass := parseaction[2]
-	actscope, _ := strconv.Atoi(parseaction[3])
-
+	ActionParser(action)
+	// fmt.Println(tokenaction.area, tokenaction.class, tokenaction.scope)
 	if kerrighedContainerVerify() {
-		switch area {
+		switch tokenaction.area {
 		case "nodes":
-			cmdNodes(actclass, actscope)
+			cmdNodes(tokenaction.class, tokenaction.scope)
 		case "cluster":
-			cmdCluster(actclass, actscope)
+			cmdCluster(tokenaction.class, tokenaction.scope)
 		default:
 			fmt.Println("Please choose the area {nodes or cluster}")
-
 		}
 	} else {
 		fmt.Println("Please Continue in Kerrighed Container")
 		return false
 	}
+	clearAction()
 	return false
 }
-
-func cmdNodes(actclass string, actscope int) interface{} {
+func clearAction() {
+	tokenaction.area = ""
+	tokenaction.class = ""
+	tokenaction.scope = nil
+}
+func cmdNodes(actclass string, actscope []string) interface{} {
 
 	switch actclass {
 	case "status":
-		verbosenode := nodeStatus(actscope)
-		fmt.Println("node", verbosenode)
+		verbosenode := nodeStatus(actscope[0])
+		fmt.Println("Node Status\n ", verbosenode, "\n++++++++++++++++")
 	case "add":
 		if checkNFS() {
-
 		} else {
 			restartNFS()
 		}
-		// nodeOnlineCheck
+		nodeStatus("0")
+
+		if nNodeAvailable(actscope[0]) {
+			addNodes(actscope[0])
+		}
 
 	case "del":
 	default:
@@ -64,12 +109,18 @@ func cmdNodes(actclass string, actscope int) interface{} {
 	return false
 }
 
-func cmdCluster(actclass string, actscope int) {
+func cmdCluster(actclass string, actscope []string) {
 
 }
 
-func addNodes(action string) {
-
+func addNodes(actscope string) interface{} {
+	cmd := exec.Command("krgadm", "nodes", "add", "-n", actscope)
+	result, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Node Can't add the Num of [", actscope, "] Node")
+		return err
+	}
+	return result
 }
 
 func checkNFS() bool {
@@ -98,22 +149,22 @@ func restartNFS() {
 
 // @ N node nodeStatus index == n
 // @ all node status index == 0
-func nodeStatus(index int) interface{} {
+func nodeStatus(index string) interface{} {
 	cmd := exec.Command("krgadm", "nodes", "status")
 	result, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("Node status error Occured!!")
 	}
 	fmt.Println("Compute ON? ", verifyNPort("192.168.110.2", "2222"))
-	if index == 0 {
-		fmt.Println("HCC All Nodes Status \nIP  status\n", result)
+	if index == "0" {
+		fmt.Println("HCC All Nodes Status \nIP  status\n", string(result))
 		nodeStatusRegister(string(result))
 		return string(result)
 	} else {
 		tmpstr := strings.Split(string(result), "\n")
 		for _, words := range tmpstr {
 			retoken := strings.Split(string(words), ":")
-			if string(words[0]) == strconv.Itoa(index) {
+			if string(words[0]) == index {
 				fmt.Println(index, " th node status = > ", retoken[1])
 				return string(retoken[1])
 			}
@@ -123,11 +174,34 @@ func nodeStatus(index int) interface{} {
 }
 
 func nodeStatusRegister(status string) {
-	tmpstr := strings.Split(string(status), "\n")
+	nodemap = make(map[string]string)
+	tmpstr := strings.Split(status, "\n")
 	for _, words := range tmpstr {
-		retoken := strings.Split(string(words), ":")
-		nodemap[string(words[0])] = retoken[1]
+		if strings.Contains(string(words), ":") {
+			retoken := strings.Split(string(words), ":")
+			if string(words[0]) != "1" {
+				nodemap[string(words[0])] = retoken[1]
+			}
+			// fmt.Println("words => ", string(words[0]), "retoken => ", retoken[1])
+			// fmt.Println("register => ", nodemap[])
+
+		}
 	}
+}
+
+// nNodeAvailable : check
+func nNodeAvailable(actscope string) bool {
+	// fmt.Println("qwe => ", nodemap["1"])
+	if nodemap[actscope] == "present" {
+		return true
+	}
+	// for key, val := range nodemap {
+	// 	if val == "online" && key != "1" {
+	// 		return false
+	// 	}
+	// 	fmt.Println("Codex => ", key, val)
+	// }
+	return true
 }
 
 func printOutput(outs string) {
@@ -183,6 +257,8 @@ func verifyNPort(ip string, port string) bool {
 	return false
 }
 
-func nodeInit() {
-	nodemap = make(map[string]string)
-}
+// NodeInit :qwe
+// func NodeInit() {
+// 	nodemap = make(map[string]string)
+// 	// nodemap = map[string]string{}
+// }
--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"hcc/viola/lib/config"
-	"hcc/viola/lib/controlcli"
 	"hcc/viola/lib/logger"
+	"strings"
 )
 
 func main() {
@@ -42,6 +43,14 @@ func main() {
 	// logger.Logger.Println(" [*] Waiting for messages. To exit press Ctrl+C")
 	// <-forever
 
-	controlcli.HccCli("hcc nodes status 0")
-	controlcli.nodeOnlineCheck()
+	// controlcli.HccCli("hcc nodes status 0")
+	// // controlcli.NodeInit()
+	// controlcli.HccCli("hcc nodes add 2")
+	// fmt.Println("Result\n")
+	// controlcli.HccCli("hcc nodes status 0")
+	// controlcli.HccCli("krgadm nodes add -n 1:2")
+	var qwe []string
+	qwe = append(qwe, "2:3")
+	asd := strings.Split(qwe[0], ":")
+	fmt.Println(asd[0], "++++++++++++++++", asd[1])
 }
