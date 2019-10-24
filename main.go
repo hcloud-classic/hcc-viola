2184587aab4ed376e7acf4efaab9087abbf4cd71
ish <ish@innogrid.com>
2019-10-25 08:58:29 +0900

    fix logic

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"hcc/viola/action/rabbitmq"
 	"hcc/viola/lib/config"
 	"hcc/viola/lib/logger"
-	"strings"
 	"time"
 )
 
@@ -41,18 +39,19 @@ func main() {
 			time.Sleep(time.Second * 3)
 			continue
 		}
-		defer func() {
-			_ = rabbitmq.Channel.Close()
-		}()
-		defer func() {
-			_ = rabbitmq.Connection.Close()
-		}()
 	}
 
-	// forever := make(chan bool)
+	forever := make(chan bool)
 
-	// logger.Logger.Println(" [*] Waiting for messages. To exit press Ctrl+C")
-	// <-forever
+	logger.Logger.Println(" [*] Waiting for messages. To exit press Ctrl+C")
+	<-forever
+
+	defer func() {
+		_ = rabbitmq.Channel.Close()
+	}()
+	defer func() {
+		_ = rabbitmq.Connection.Close()
+	}()
 
 	// controlcli.HccCli("hcc nodes status 0")
 	// // controlcli.NodeInit()
@@ -60,8 +59,4 @@ func main() {
 	// fmt.Println("Result\n")
 	// controlcli.HccCli("hcc nodes status 0")
 	// controlcli.HccCli("krgadm nodes add -n 1:2")
-	var qwe []string
-	qwe = append(qwe, "2:3")
-	asd := strings.Split(qwe[0], ":")
-	fmt.Println(asd[0], "++++++++++++++++", asd[1])
 }
